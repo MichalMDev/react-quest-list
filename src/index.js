f7f157412d723ee0b1928b82686f10d0a2a1ce41
MichalMDev <34262958+MichalMDev@users.noreply.github.com>
2019-12-06 18:49:50 +0100

    06.12

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-var cors = require('cors') 
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+var cors = require("cors");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-app.use(cors())
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+app.use(cors());
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-//     const task = await Task.findById('5c2e505a3253e18a43e612e6')
-//     await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('5dcfdbfeb7cd2023b89af5e3')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
+const Task = require("./models/task");
+const User = require("./models/user");
